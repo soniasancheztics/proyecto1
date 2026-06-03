34c997ef4cef814ef391fb2096a6d3b4b481803b
soniasancheztics <60482432+soniasancheztics@users.noreply.github.com>
2026-06-03 17:25:54 +0200

    Update header text in index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Mi primer proyecto Git</h1>
+    <h1>Mi primer proyecto Git con php</h1>
     <h2>Comandos</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus rerum, odio inventore vitae molestiae enim vel ea consequuntur illum? Sed laboriosam voluptatem voluptas. Odit animi facere vel quas, voluptate aspernatur!</p>
     <h2>Flujo de trabajo</h2>
