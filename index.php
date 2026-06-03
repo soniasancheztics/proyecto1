cae9ea8b4c46ccab063d48786add48bce0ae0b2b
Sonia <soniasancheztics@gmail.com>
2026-06-03 17:30:05 +0200

    proyecto2 final

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Mi primer proyecto Git con php</h1>
-
+<h2>Subtítulo</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus rerum, odio inventore vitae molestiae enim vel ea consequuntur illum? Sed laboriosam voluptatem voluptas. Odit animi facere vel quas, voluptate aspernatur!</p>
     <h2>Flujo de trabajo</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quaerat veritatis nam distinctio laborum illum sed ut ipsa perspiciatis reiciendis. Libero, officiis necessitatibus reiciendis quo amet esse incidunt vitae distinctio?</p>
